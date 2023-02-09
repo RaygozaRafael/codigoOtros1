e67da8391deee2d4efcf2e42f8174f560063f96f
RaygozaRafael <raygoza.zamudio.rafael@gmail.com>
2023-02-09 17:35:13 -0600

    Codigo Otros 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,31 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) {/* Se agrego la propiedad async antes de function */
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  /* Se agrego .then para la conversion a tipo json lo recuperado */
+  const response = await fetch(`${usersEndpoint}/${username}`)
+  .then(response => response.json())
+  .then(json => {
+    /* Se cambio el tipo data a json */
+    console.log(json);
+    $n.textContent = `${json.name}`;
+    $b.textContent = `${json.blog}`;
+    $l.textContent = `${json.location}`;
+  })
+  //const data = response.map();
+  
+  
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  /* n a $n */
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
